180fe8e8e39456ef8629e2d717a2e27aa32e4b43
adotahirovich <80359702+adotahirovich@users.noreply.github.com>
2022-07-14 01:19:41 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$hostname = "us-cdbr-east-05.cleardb.net";
+$username = "b8d0fd457c8151";
+$password = "7b165f3a";
+$database = "heroku_1968f4761ed83f6";
+
+$conn = mysqli_connect ($hostname, $username, $password, $database) or die("Database connection failed ! !");
+
+
+?>
